3400e9a3445911aff1e2b438279a074f21543887
babydoja <oladojabolatito29@gmail.com>
2025-08-14 10:03:08 +0100

    recommit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,18 @@ const PORT = 5000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors({  origin:["http://localhost:5173","https://blog-steel-one-91.vercel.app/"] }))
-app.use(router)
+// app.use(cors({  origin:["http://localhost:5173","https://blog-steel-one-91.vercel.app/"] }))
+// app.use(router)
+
+app.use(cors(
+    {
+        origin:["http://localhost:5173"]
+    }
+))
+app.use('/api/blog',router)
 mongoose.connect(process.env.MONGO_URL)
+
+
 .then((req,res)=>{
    app.listen(PORT,()=>{
     console.log("Connected to MongoDB")
@@ -24,4 +33,7 @@ mongoose.connect(process.env.MONGO_URL)
 .catch((err)=>{
     console.log(err);
 })
+
+
+
 
